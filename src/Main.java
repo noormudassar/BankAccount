4a26e1ee1b23337ccd35bb8b84fa093eb0911e7a
Noor Mudassar <noormudassar97@gmail.com>
2023-09-01 11:49:06 -0400

    added savings balance method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static class BankAccount {
-        public double checkingSavings;
+        public double checkingBalance;
         public double savingsBalance;
         public static int numberOfAccounts = 0;
         public static double totalAmountStored = 0;
@@ -8,6 +8,23 @@ public class Main {
         public BankAccount () {
             numberOfAccounts++;
         }
+        public double getCheckingBalance() {
+            return checkingBalance;
+        }
+        public double getSavingsBalance() {
+            return savingsBalance;
+        }
+        public void deposit (double money, String accountType) {
+                if ("checking".equalsIgnoreCase(accountType)) {
+                    checkingBalance+=money;
+                } else if ("savings".equalsIgnoreCase(accountType)) {
+                    savingsBalance+=money;
+            }
+                totalAmountStored+=money;
+        }
+    }
+    public void withdraw (double money, String accountType) {
+
     }
 }
 
