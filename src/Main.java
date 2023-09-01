7410ef2b6180bb9742e6a1e797b8aba57b95b882
Noor Mudassar <noormudassar97@gmail.com>
2023-09-01 12:10:51 -0400

    added withdraw balance method

--- src/Main.java
+++ src/Main.java
@@ -22,9 +22,23 @@ public class Main {
             }
                 totalAmountStored+=money;
         }
-    }
-    public void withdraw (double money, String accountType) {
-
+        public void withdraw (double money, String accountType) {
+            if (checkingBalance >= money && "checking".equalsIgnoreCase(accountType)) {
+                checkingBalance -= money;
+                totalAmountStored -= money;
+            } else if (savingsBalance >= money && "savings".equalsIgnoreCase((accountType))) {
+                savingsBalance -= money;
+                totalAmountStored -= money;
+            } else {
+                System.out.println("Cannot withdraw money. Insufficient funds");
+            }
+        }
+        public static double getTotalAmountStored() {
+            return totalAmountStored;
+        }
+        public static int getNumberOfAccounts() {
+            return numberOfAccounts;
+        }
     }
 }
 
